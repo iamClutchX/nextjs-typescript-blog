d1edc7d591bcafe2960064e02758a8e28daf4fd4
iamClutchX <iamr.karan@gmail.com>
2023-01-31 15:44:13 +0530

    pagesize

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
         sort: ['id:desc'],
         pagination: {
             page: query.page ? +query.page : 1,
-            // pageSize: 1,
+            pageSize: 1,
         },
     };
 
